263605767b6f429436d4ae95df952a678691cc5f
liua13 <liua1@bxscience.edu>
2021-08-19 18:41:17 -0400

    added modal for sponsor blurb

--- src/components/sponsors.js
+++ src/components/sponsors.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 import styled from 'styled-components';
 import BackgroundImage from 'gatsby-background-image';
+import ReactModal from "react-modal";
 
 import Image from "./subcomponents/image";
 
 import byteTelescope from "../images/byteTelescope.png";
 import sponsorsData from "../data/sponsors.json";
+
+ReactModal.setAppElement("#___gatsby");
 
 const BackgroundSection = ({className}) => {
   const data = useStaticQuery(
@@ -25,6 +28,24 @@ const BackgroundSection = ({className}) => {
 
   const imageData = data.desktop.childImageSharp.fluid;
 
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [sponsorName, setSponsorName] = useState("");
+  const [sponsorLink, setSponsorLink] = useState("");
+  const [sponsorBlurb, setSponsorBlurb] = useState("");
+
+  const openModal = (name, link, blurb) => {
+    if (blurb) {
+      setSponsorName(name);
+      setSponsorLink(link);
+      setSponsorBlurb(blurb);
+      setIsModalOpen(true);
+    }
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <BackgroundImage
       className={className}
@@ -34,28 +55,40 @@ const BackgroundSection = ({className}) => {
       <section id="sponsors">
           <div className="sponsorsSubcontainer">
             <h2>Our Sponsors</h2>
+            <h3>Click on each of our sponsors to learn more!</h3>
 
-            {/* <div className="sponsorLogoContainer">
-              {sponsorsData.map((data) => (
-                  <div className="sponsorLogo" key={data.sponsor}>
-                    <a href={data.link} target="_blank" rel="noreferrer">
-                      <Image filename={data.logo} alt="byte-images" />
-                    </a>
-                  </div>
-              ))}
-            </div> */}
+            <ReactModal
+              isOpen={isModalOpen}
+              onRequestClose={closeModal}
+              contentLabel="Sponsor Blurbs"
+              className="Modal"
+              overlayClassName="Overlay"
+              scrollable={true}
+            >
+              <div style={{ textAlign: "right" }}>
+                <p style={{ color: "black", cursor: "pointer", display: "inline", fontSize: "30px" }} onClick={closeModal}>x</p>
+              </div>
+              <h1 style={{ marginBottom: "25px" }}>
+                {(() => {
+                  switch (sponsorLink) {
+                    case "":   return sponsorName;
+                    default:   return <a style={{color: "#707AD9"}} href={sponsorLink} target="_blank">{sponsorName}</a>
+                  }
+                })()}
+              </h1>
+              <div dangerouslySetInnerHTML={{ __html: sponsorBlurb }}></div>
+            </ReactModal>
 
             <div className="sponsorLogoContainer">
               {Array.from(Array(10).keys()).map(index => 
-                <div className="sponsorLogoSubcontainer">
+                <div className="sponsorLogoSubcontainer" key={index}>
                   {sponsorsData.map((data) => {
                     if (index === data.tier){
                       return (
-                      <div className="sponsorLogo" key={data.sponsor}>
-                        <a href={data.link} target="_blank" rel="noreferrer">
+                      <div className="sponsorLogo" key={data.sponsor} onClick={() => openModal(data.sponsor, data.link, data.blurb)}>
+                        {/* <a href={data.link} target="_blank" rel="noreferrer"> */}
                           <Image filename={data.logo} alt="byte-images" />
-                        </a>
-                        {/* {data.sponsor} */}
+                        {/* </a> */}
                       </div>
                       )
                     }
